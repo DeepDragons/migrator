791837f5548b9e8cfd92dd3f582944923d1c46f5
Rinat <lich666black@gmail.com>
2020-12-10 17:25:29 +0500

    added test methods

--- index.js
+++ index.js
@@ -1,42 +1,25 @@
 const { Zilliqa } = require('@zilliqa-js/zilliqa');
-const { BN, Long } = require('@zilliqa-js/util');
+const { BN, bytes, Long } = require('@zilliqa-js/util');
+const { RPCMethod } = require('@zilliqa-js/core');
+const { fromBech32Address } = require('@zilliqa-js/crypto');
+const _ = require('lodash');
 
 const node = 'https://api.zilliqa.com';
-
-const maincontractFrom = 'zil1epndtz8lhv7lemwfrnadvwftmtaruclkdg6fee';
-const maincontractTo = '';
+const MSG_VERSION = 1;
+const maincontractFrom = fromBech32Address('zil1epndtz8lhv7lemwfrnadvwftmtaruclkdg6fee');
+const maincontractTo = fromBech32Address('zil1zw5r4c5ygmhahe6shfp4x6alee0cug00jj7t63');
 const transition = 'GiveBirth';
 const privateKey = process.env.KEY;
 
-[{
-  "vname": "dragons",
-  "type": "List (Dragon)",
-  "value": [
-    {
-      "constructor": "Dragon",
-      "argtypes": [
-        "Uint256",
-        "ByStr20",
-        "Uint32",
-        "String",
-        "Uint256",
-        "Uint256"
-      ],
-      "arguments": [
-        "2", //token_id
-        "0x0Ebe43B507467b0F6B5E206Ad58b8A3fc5Ba0C19", // owner
-        "1", // stage
-        "https://res.cloudinary.com/dragonseth/image/upload/0_1535.png", // token_uri
-        "68472021902440675240152540012429795118111927856965723433540267169483745008602", // token_gen_image
-        "77700130301403230241121055224" // token_gen_battle
-      ]
-    }
-  ]
-}]
+function sleap(value) {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(), value);
+  }, []);
+}
 
 function getState(stage) {
-  if (stage > 2) {
-    return 2;
+  if (stage > 1) {
+    return 1;
   }
 
   return stage;
@@ -47,56 +30,84 @@ function geturl(id, stage) {
   return `https://res.cloudinary.com/dragonseth/image/upload/${stage}_${id}.png`;
 }
 
-async function main() {
+async function getContract() {
   const zilliqa = new Zilliqa(node);
   const { result } = await zilliqa.blockchain.getSmartContractState(maincontractFrom);
+
+  return result;
+}
+
+async function main() {
+  const zilliqa = new Zilliqa('https://dev-api.zilliqa.com');
   const {
     token_gen_battle,
     token_gen_image,
     token_owners,
-    token_uris,
     tokens_owner_stage
-  } = result;
+  } = await getContract();
+  const netID = await zilliqa.network.GetNetworkId();
+  const version = bytes.pack(netID.result, MSG_VERSION);
   const gasLimit = Long.fromNumber(9000);
   const gasPrice = new BN('2000000000');
   const amount = new BN(0);
   const toAddr = maincontractTo;
-  // const pubKey = zilliqa.wallet.defaultAccount.publicKey;
+  const values = Object.keys(token_owners).map((tokenID) => ({
+    constructor: 'Dragon',
+    argtypes: [
+      'Uint256',
+      'ByStr20',
+      'Uint32',
+      'String',
+      'Uint256',
+      'Uint256'
+    ],
+    arguments: [
+      tokenID, //token_id
+      String(token_owners[tokenID]).toLowerCase(), // owner
+      getState(tokens_owner_stage[token_owners[tokenID]][tokenID]), // stage
+      geturl(tokenID, tokens_owner_stage[token_owners[tokenID]][tokenID]), // token_uri
+      token_gen_image[tokenID], // token_gen_battle
+      token_gen_battle[tokenID] // token_gen_image
+    ]
+  }));
+  zilliqa.wallet.addByPrivateKey(privateKey);
+  const chunks = _.chunk(values, 1);
+  const balance = await zilliqa.blockchain.getBalance(zilliqa.wallet.defaultAccount.address);
+  let nonce = balance.result.nonce;
+  const pubKey = zilliqa.wallet.defaultAccount.publicKey;
 
-  const data = {
-    _tag: transition,
-    params: [{
-      "vname": "dragons",
-      "type": "List (Dragon)",
-      "value": Object.keys(token_owners).map((tokenID) => (    {
-        "constructor": "Dragon",
-        "argtypes": [
-          "Uint256",
-          "ByStr20",
-          "Uint32",
-          "String",
-          "Uint256",
-          "Uint256"
-        ],
-        "arguments": [
-          tokenID, //token_id
-          token_owners[tokenID], // owner
-          getState(tokens_owner_stage[token_owners[tokenID]][tokenID]), // stage
-          geturl(tokenID, tokens_owner_stage[token_owners[tokenID]][tokenID]), // token_uri
-          token_gen_battle[tokenID], // token_gen_image
-          token_gen_image[tokenID] // token_gen_battle
-        ]
-      }))
-    }]
+  for (let index = 0; index < chunks.length; index++) {
+    nonce++;
+
+    const chunk = chunks[index];
+    const data = JSON.stringify({
+      _tag: transition,
+      params: [{
+        vname: 'dragons',
+        type: 'List (Dragon)',
+        value: chunk
+      }]
+    });
+
+    const zilTxData = zilliqa.transactions.new({
+      data,
+      nonce,
+      version,
+      amount,
+      gasPrice,
+      gasLimit,
+      toAddr,
+      pubKey
+    });
+    const { txParams } = await zilliqa.wallet.sign(zilTxData);
+    const tx = await zilliqa.provider.send(
+      RPCMethod.CreateTransaction,
+      txParams
+    );
+
+    console.log(JSON.stringify(tx, null, 4));
+    break;
   }
-  // const { txParams } = await zilliqa.wallet.sign(zilTxData);
-  // const tx = await zilliqa.provider.send(
-  //   RPCMethod.CreateTransaction,
-  //   txParams
-  // );
-
-
-  console.log(JSON.stringify(data, null, 4));
 }
 
 main();
